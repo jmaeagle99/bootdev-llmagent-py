3e88539eab64c375b2343261603f86d7ab9b3426
jmaeagle99 <44687433+jmaeagle99@users.noreply.github.com>
2025-09-05 11:33:34 -0700

    Get prompt from command line args

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        contents=user_prompt,
     )
 
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
