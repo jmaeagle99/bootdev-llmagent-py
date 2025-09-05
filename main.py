589c32e46023fabbc726563b65799933c632e8dc
jmaeagle99 <44687433+jmaeagle99@users.noreply.github.com>
2025-09-05 11:44:32 -0700

    Add verbosity flag

--- main.py
+++ main.py
@@ -2,28 +2,43 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
-    args = sys.argv[1:]
+    isVerbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: python main.py "your prompt here"')
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
+
     user_prompt = " ".join(args)
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    if isVerbose:
+        print(f"User prompt: {user_prompt}")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=user_prompt,
+        contents=messages,
     )
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if isVerbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
     print("Response:")
     print(response.text)
 
